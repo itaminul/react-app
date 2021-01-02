cf9eec51703b9f09ebc81ce35bc6b99386cf958a
itaminul <itaminul@gmail.com>
2021-01-03 00:40:11 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './components/assets/vendor/bootstrap/css/bootstrap.min.css'
+import './components/assets/vendor/icofont/icofont.min.css'
+import './components/assets/vendor/boxicons/css/boxicons.min.css'
+import './components/assets/vendor/animate.css/animate.min.css'
+import './components/assets/vendor/owl.carousel/assets/owl.carousel.min.css'
+import './components/assets/vendor/venobox/venobox.css'
+import './components/assets/css/style.css'
+
+import { Provider } from 'react-redux'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
